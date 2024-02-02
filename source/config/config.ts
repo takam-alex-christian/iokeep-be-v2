380275fd5cb954cec8f9e587f8b2509214a8c904
takam-alex-christian <takamalexchristian@gmail.com>
2024-02-02 12:45:21 +0800

    config params set with env variable values and default values

--- source/config/config.ts
+++ source/config/config.ts
@@ -1,5 +1,5 @@
 
-const port = process.env.PORT || 8000
-const mongodb_uri = process.env.MONGODB_URI || ""
+const port = process.env.PORT || 4000
+const mongodb_uri = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/iokeepv2dev?"
 
 export {port, mongodb_uri}
