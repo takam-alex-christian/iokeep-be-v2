91f0cd3dd1ffc05e91cc1184ba56e48d27f90045
takam-alex-christian <takamalexchristian@gmail.com>
2024-02-02 13:15:41 +0800

    userModel implemented and exported

--- source/features/userAuthentication/auth.model.ts
+++ source/features/userAuthentication/auth.model.ts
@@ -6,5 +6,11 @@ import { Schema, model } from "mongoose"
 
 const userSchema = new Schema({
     userId: String,
-    username: String
+    username: String,
+    password: String,
+    creationDate: Date,
+}, {
+    _id: false
 })
+
+export default model("User", userSchema)
