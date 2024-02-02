6efb51669593eb766b5158a5e03905c08d62491e
takam-alex-christian <takamalexchristian@gmail.com>
2024-02-03 00:45:39 +0800

    password hashed with bcrypt before saving to db and some code refactoring

--- source/features/userAuthentication/auth.services.ts
+++ source/features/userAuthentication/auth.services.ts
@@ -1,29 +1,66 @@
 
-
+import { hash } from 'bcrypt'
 
 import UserModel from "./auth.model"
 
 
-//a function to create user
-async function createUser({username, password}: {username: string, password: string}){
-
-
-
-    //here we hash the password,
-    
-
-    
-
-    if(process.env.NODE_ENV === 'development'){
-        console.log(`createUser called time: ${Date.now().toLocaleString()}`)
-    }
-
-    return new UserModel({
-        username,
-        password
-    }).save()
-
-    
+async function doesUsernameExist({ username }: { username: string }): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+        UserModel.findOne({ username }).then((userDoc) => {
+            if (userDoc) resolve(true)
+            else resolve(false)
+        }).catch((err) => {
+            reject(err)
+        })
+    })
 }
 
-export {createUser}
+//a function to create user
+async function createUser({ username, password }: { username: string, password: string }) {
+
+    //check if username exist first
+
+    const saltRounds = 10
+
+    const createUserPromise = new Promise((createUserResolve, createUserReject) => {
+
+        doesUsernameExist({ username }).then(async (userExist) => {
+
+            if (!userExist) {
+
+                await hash(password, saltRounds).then((generatedHash) => {
+
+                    return createUserResolve(new UserModel({
+                        username,
+                        password: generatedHash
+                    }).save())
+
+                }, (err) => {
+                    //handle problem with hashing the password
+                    if (process.env.NODE_ENV === 'development') {
+                        console.log(`error while hashing user password, for more info-> ${err}`);
+                    }
+                    
+                    return createUserReject(err)
+                })
+                
+            } else {
+                createUserReject(new Error("username_unavailable"))
+            }
+        }, (err) => {
+            createUserReject(err)
+        })
+    })
+
+
+
+
+    if (process.env.NODE_ENV === 'development') {
+        console.log(`createUser called time: ${new Date(Date.now()).toISOString()}`)
+    }
+
+
+    return createUserPromise
+}
+
+export { createUser }
