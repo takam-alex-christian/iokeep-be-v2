7b976c099330238f312993a344b3cdf90a5507fe
takam-alex-christian <takamalexchristian@gmail.com>
2024-02-02 12:44:18 +0800

    db connection implemented

--- source/config/db.ts
+++ source/config/db.ts
@@ -1,4 +1,30 @@
 
-
+import { connect, connection } from "mongoose"
 //connect to db
+
+import { mongodb_uri } from "./config"
+
+
+
+export default function connectMongoDb() {
+    connect(mongodb_uri).catch((e) => {
+        //log this into a file somewhere
+        console.log(e)
+    })
+
+
+    connection.once("open", ()=>{
+        
+       if (process.env.NODE_ENV === 'development'){
+        console.log("MongoDb connected")
+       }
+    })
+
+    connection.on("error", (e)=>{
+        if (process.env.NODE_ENV === 'production'){
+            console.log("Error on mongodb")
+        }
+    })
+}
+
 
