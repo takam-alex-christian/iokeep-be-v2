9d1583c047fd53ad6f5fbcebef3c831015d7f4f8
takam-alex-christian <takamalexchristian@gmail.com>
2024-02-02 13:14:25 +0800

    auth controller implemented with blank controller functions

---
+++ source/features/userAuthentication/auth.controller.ts
@@ -0,0 +1,18 @@
+
+import {Request, Response, NextFunction} from "express"
+
+
+
+async function loginController(req: Request, res: Response, next: NextFunction){
+
+    
+
+}
+
+
+async function signupController(req: Request, res: Response, next: NextFunction){
+
+}
+
+
+export {loginController, signupController}
