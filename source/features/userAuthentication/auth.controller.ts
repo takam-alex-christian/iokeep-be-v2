3fb185eb58ba01b835574b82625af49237c1c349
takam-alex-christian <takamalexchristian@gmail.com>
2024-02-03 13:08:51 +0800

    loginController partially implemented

--- source/features/userAuthentication/auth.controller.ts
+++ source/features/userAuthentication/auth.controller.ts
@@ -1,12 +1,21 @@
 
 import { Request, Response, NextFunction } from "express"
 
-import { createUser } from "./auth.services"
+import { createUser, authUser } from "./auth.services"
 
 
 
 async function loginController(req: Request, res: Response, next: NextFunction) {
+    if (req.body.username && req.body.password){
+        //handle auth
+        
+        await authUser({username: req.body.username, password: req.body.password}).then((authed)=>{
+            res.status(200).send("user logged in")
+        })
 
+    }else {
+        res.status(400).send("Bad Request! ")
+    }
 }
 
 
@@ -15,17 +24,16 @@ async function signupController(req: Request, res: Response, next: NextFunction)
     if (req.body.username && req.body.password) {
 
         await createUser({ username: req.body.username, password: req.body.password }).then((userDocument) => {
-            
+
             if (userDocument) {
                 // user successfully created
-                res.status(201).send("new user created")
+                res.status(201).send("new user created") // if status is 201, we send a json with user token to the user
             }
         }, (err) => {
             // what to do if there's an error
             if (process.env.NODE_ENV === "development") {
                 console.log(`error caught on model: ${err}`)
             }
-
 
             //handle for username_unavailable errors
 
