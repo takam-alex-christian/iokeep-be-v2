c6a5bc662ae26992c7f7a2300f68038b6d28a14f
takam-alex-christian <takamalexchristian@gmail.com>
2024-02-04 01:26:00 +0800

    the access_token route implemented

--- source/features/userAuthentication/auth.route.ts
+++ source/features/userAuthentication/auth.route.ts
@@ -2,12 +2,14 @@
 
 import {Router} from "express"
 
-import {signupController, loginController} from "./auth.controller"
+import {signupController, loginController, getAccessTokenController} from "./auth.controller"
 
 
 const authRouter = Router()
 
 authRouter.post("/signup", signupController)
 authRouter.post("/login", loginController)
+
+authRouter.get("/access_token", getAccessTokenController )
 
 export default authRouter
