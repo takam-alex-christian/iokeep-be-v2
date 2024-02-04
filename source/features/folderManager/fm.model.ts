29917d007924a7b2e0ef25cef8a380be6405e105
takam-alex-christian <takamalexchristian@gmail.com>
2024-02-04 16:38:13 +0800

    folderModel implemented

---
+++ source/features/folderManager/fm.model.ts
@@ -0,0 +1,44 @@
+
+
+import {Schema, model, mongo} from "mongoose"
+
+
+const folderSchema = new Schema({
+    _id: {
+        type: mongo.ObjectId,
+        default: ()=>{
+            return new mongo.ObjectId()
+        }
+    },
+    ownerId: {
+        type: mongo.ObjectId,
+        require: true
+    },
+    folderName: {
+        type: String,
+        require: true,
+
+    },
+    creationDate: { //as soon as the folder is created, provide this
+        type: Date,
+        default: ()=>{
+            return Date.now()
+        }
+    },
+    lastOpenedDate: { // if the folder is consulted, update this
+        type: Date,
+        default: ()=>{
+            return Date.now()
+        }
+    },
+    lastModifiedDate: { //if there are operations within the folder, update this
+        type: Date,
+        default: ()=>{
+            return Date.now()
+        }
+    }
+})
+
+const FolderModel = model("Folder", folderSchema);
+
+export {FolderModel}
