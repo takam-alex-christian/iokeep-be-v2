acbc9eff05070684c550ed287f1d5952b822dd90
takam-alex-christian <takamalexchristian@gmail.com>
2024-02-04 16:39:17 +0800

    createFolder service implemented

---
+++ source/features/folderManager/fm.services.ts
@@ -0,0 +1,45 @@
+
+
+
+import { FolderModel } from "./fm.model";
+
+
+
+// create folder
+// read folders
+// update folder
+// delete folder
+
+function createFolder({ ownerId, folderName }: { ownerId: string, folderName: string }): Promise<{ folderId: string }> {
+    return new Promise((createFolderResolve, createFolderReject) => {
+
+        // ensure no folder with this foldername is owned by a user of this userid
+
+        FolderModel.findOne({ folderName, ownerId }).then((folderDoc) => {
+
+            if (!folderDoc) {
+
+                // create folder
+
+                new FolderModel({ ownerId, folderName }).save().then((folderDoc) => {
+
+                    createFolderResolve({ folderId: folderDoc._id.toString()})
+                    // console.log("got here though ?")
+
+                }, (err) => { // mongoose error
+                    createFolderReject(err)
+                })
+
+            } else {
+                createFolderReject(new Error("A folder Already exists with this folder name"))
+            }
+
+        }, (err) => {
+            createFolderReject(err)
+        })
+
+        console.log("got here expectedly")
+    })
+}
+
+export { createFolder }
