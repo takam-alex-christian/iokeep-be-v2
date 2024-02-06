3afdad6340adff8d1460b7b6c0a8258fcd559dcf
takam-alex-christian <takamalexchristian@gmail.com>
2024-02-06 16:59:56 +0800

    read note editorstate && deleteNote services implemented

--- source/features/noteManager/nm.services.ts
+++ source/features/noteManager/nm.services.ts
@@ -10,36 +10,64 @@ import { NoteModel } from "./nm.model";
 //delete note
 
 
-function createNote(folderId: string, editorState: string): Promise<string>{
-    return new Promise((createNoteResolve, createNoteReject)=>{
+function createNote(folderId: string, editorState: string): Promise<string> {
+    return new Promise((createNoteResolve, createNoteReject) => {
 
-        new NoteModel({folderId, editorState}).save().then((noteDoc)=>{
+        new NoteModel({ folderId, editorState }).save().then((noteDoc) => {
             createNoteResolve(noteDoc._id.toString())
-        }, (err)=>{
+        }, (err) => {
             createNoteReject(err)
         })
 
     })
 }
 
-function readNotes(folderId: string): Promise<Array<any>>{
-    return new Promise((readNotesResolve, readNotesReject)=>{
+function readNotes(folderId: string): Promise<Array<any>> {
+    return new Promise((readNotesResolve, readNotesReject) => {
 
-        NoteModel.find({folderId}).then((notes)=>{
+        NoteModel.find({ folderId }).then((notes) => {
             readNotesResolve(notes)
-        }, (err)=>{
+        }, (err) => {
             readNotesReject(err)
         })
 
     })
 }
 
-function readNoteEditorState(){
-
+function readNoteEditorState(noteId: string): Promise<string> { //returns editorState string
+    //
+    return new Promise((readNoteRsolve, readNoteReject) => {
+        NoteModel.findById(noteId).then((noteDoc) => {
+            if (noteDoc) {
+                readNoteRsolve(noteDoc.editorState)
+            } else {
+                readNoteReject(new Error("inexistent noteId"))
+            }
+        }, (err) => {
+            readNoteReject(err)
+        })
+    })
 }
 
-function deleteNote(){
+//function to update editorState
 
+function deleteNote(noteId: string): Promise<boolean>{
+    return new Promise((deleteNoteResolve, deleteNoteReject) => {
+        NoteModel.findById(noteId).then((noteDoc) => {
+            if (noteDoc) {
+                //we can proceed to delete noteDoc
+                noteDoc.deleteOne().then((deleteResult) => {
+
+                    deleteNoteResolve(deleteResult.acknowledged)
+
+                }, (err) => {
+                    deleteNoteReject(err)
+                })
+            } else {
+                deleteNoteReject(new Error("inexistent note id"))
+            }
+        })
+    })
 }
 
-export {createNote, readNotes}
+export { createNote, readNotes, deleteNote}
