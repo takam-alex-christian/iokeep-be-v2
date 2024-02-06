8030193214d2b2abcf5d2f544a0f8d7793a58aeb
takam-alex-christian <takamalexchristian@gmail.com>
2024-02-06 16:35:34 +0800

    post, get routes implemented to create and read notes

--- source/features/noteManager/nm.routes.ts
+++ source/features/noteManager/nm.routes.ts
@@ -3,10 +3,15 @@ import {Router} from "express"
 
 import {checkAccessToken} from "../../customMiddleware/checkAccessToken"
 
+import {createNoteController,readNotesController } from "./nm.controllers"
+
 const nmRouter = Router()
 
 nmRouter.use(checkAccessToken)
 
+nmRouter.post("/", createNoteController)
+nmRouter.get("/", readNotesController)
 
 
-export {nmRouter}
+
+export default nmRouter
