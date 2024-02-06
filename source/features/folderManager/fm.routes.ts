d9f3a41791b955dd163ee7ab5df621626221a905
takam-alex-christian <takamalexchristian@gmail.com>
2024-02-06 14:32:52 +0800

    delete route implemented

--- source/features/folderManager/fm.routes.ts
+++ source/features/folderManager/fm.routes.ts
@@ -1,5 +1,5 @@
 
-import { createFolderController, readFoldersController, updateFolderController } from "./fm.controllers";
+import { createFolderController, deleteFolderController, readFoldersController, updateFolderController } from "./fm.controllers";
 import {checkAccessToken} from './fm.middlewares'
 
 import { Router } from "express";
@@ -12,6 +12,8 @@ fmRouter.post("/", checkAccessToken, createFolderController);
 fmRouter.get("/", checkAccessToken, readFoldersController);
 
 fmRouter.patch("/:folderId", checkAccessToken, updateFolderController);
+
+fmRouter.delete("/:folderId", checkAccessToken, deleteFolderController);
 
 
 export default fmRouter
