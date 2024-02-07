925436c4065d9d30d71a5d9d331981065025bae0
takam-alex-christian <takamalexchristian@gmail.com>
2024-02-08 02:18:05 +0800

    improving checkAccessToken middleware

--- source/customMiddleware/checkAccessToken.ts
+++ source/customMiddleware/checkAccessToken.ts
@@ -7,7 +7,7 @@ import {user_auth_key} from '../config/config';
 
 
 function checkAccessToken(req: Request, res: Response, next: NextFunction){
-    if (req.cookies["access_token"]){
+    if (req.cookies["access_token"] && req.cookies["refresh_token"]){
 
         //@ts-ignore
         verify(req.cookies["access_token"], user_auth_key, (err, decodedPayload)=>{
