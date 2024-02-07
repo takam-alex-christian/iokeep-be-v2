3fd5fd83bc67c43b1d3790855553f3ec8aace482
takam-alex-christian <takamalexchristian@gmail.com>
2024-02-08 02:42:51 +0800

    logout route implemented

--- source/features/userAuthentication/auth.route.ts
+++ source/features/userAuthentication/auth.route.ts
@@ -2,13 +2,16 @@
 
 import {Router} from "express"
 
-import {signupController, loginController, getAccessTokenController, verifyAccessTokenController} from "./auth.controller"
+import {signupController, loginController, getAccessTokenController, verifyAccessTokenController, logoutController} from "./auth.controller"
+import { checkAccessToken } from "../../customMiddleware/checkAccessToken"
 
 
 const authRouter = Router()
 
 authRouter.post("/signup", signupController)
 authRouter.post("/login", loginController)
+
+authRouter.patch("/logout", checkAccessToken, logoutController)
 
 authRouter.post("/access_token", verifyAccessTokenController)
 authRouter.get("/access_token", getAccessTokenController )
