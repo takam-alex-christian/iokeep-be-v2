68bc9b1e90b9f706d5dce618aa57a9186fe9fdc5
takam-alex-christian <takamalexchristian@gmail.com>
2024-02-15 16:56:01 +0800

    custome notes response types

---
+++ source/features/noteManager/types.ts
@@ -0,0 +1,35 @@
+
+type CreateNoteJsonResponse = {
+    success: boolean,
+    data: {
+        _id: string
+    },
+    error: null | {message: string},
+    timeStamp: number
+}
+
+type GenericNoteJsonResponse = { //patch & delete response
+    success: boolean,
+    info: string,
+    error: null | {message: string},
+    timeStamp: number
+}
+
+type SingleNoteJsonResponse = {//get one note
+    data: null | {
+        _id: string,
+        editorState: string,
+        creationDate: string
+    },
+    error: null | {message: string},
+    timeStamp: number
+
+}
+
+type MultiNoteJsonResponse = { //get many notes
+    data: Array<{_id: string, editorState: string, creationDate: string, lastModified: string}>
+    error: null | {message: string},
+    timeStamp: number
+}
+
+export type {CreateNoteJsonResponse, GenericNoteJsonResponse, SingleNoteJsonResponse, MultiNoteJsonResponse}
