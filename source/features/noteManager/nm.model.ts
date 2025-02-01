b226a0ea654ef051bd8906590e01105c98bf0a94
takam-alex-christian <takamalexchristian@gmail.com>
2025-02-01 15:33:01 +0800

    updated to include isPublic path on schema

--- source/features/noteManager/nm.model.ts
+++ source/features/noteManager/nm.model.ts
@@ -1,48 +1,46 @@
-
-
-import {Schema, model, mongo, } from 'mongoose'
-
-
+import { Schema, model, mongo } from "mongoose";
 
 const noteSchema = new Schema({
-    _id: {
-        type: mongo.ObjectId,
-        default: ()=>{
-            return new mongo.ObjectId()
-        }
+  _id: {
+    type: mongo.ObjectId,
+    default: () => {
+      return new mongo.ObjectId();
     },
-    folderId: {
-        type: mongo.ObjectId,
-        required: true
-    },
-    editorState: { 
-        type: String,
-        required: true
-    },
-    description:[String],
+  },
+  folderId: {
+    type: mongo.ObjectId,
+    required: true,
+  },
+  isPublic: {
+    type: Boolean,
+    default: false,
+  },
+  editorState: {
+    type: String,
+    required: true,
+  },
+  description: [String],
 
-    creationDate: {
-        type: Date,
-        default: ()=>{
-            return new Date()
-        }
+  creationDate: {
+    type: Date,
+    default: () => {
+      return new Date();
     },
-    lastOpenedDate: {
-        type: Date,
-        default: ()=>{
-            return new Date()
-        }
+  },
+  lastOpenedDate: {
+    type: Date,
+    default: () => {
+      return new Date();
     },
-    lastModifiedDate: {
-        type: Date,
-        default: ()=>{
-            return new Date()
-        }
+  },
+  lastModifiedDate: {
+    type: Date,
+    default: () => {
+      return new Date();
     },
-    
-
-})
+  },
+});
 
 const NoteModel = model("Note", noteSchema);
 
-export {NoteModel}
+export { NoteModel };
